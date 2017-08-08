78db2cc7002022fb5f5a7d8479fb8d067781be7e
John Cook <john.b.cook05@gmail.com>
2017-08-08 08:21:24 -0600

    not working

--- quiz.js
+++ quiz.js
@@ -1,12 +1,14 @@
 
 
 
-function Quiz(questions) {
+function Quiz(questions, callback) {
     console.log("in quiz")
     console.log(questions.length)
     this.score = 0;
     this.questions = questions;
     this.currentQuestionIndex = 0;
+
+    
     
 }
 
@@ -25,51 +27,53 @@ Quiz.prototype.hasEnded = function() {
     return this.currentQuestionIndex >= this.questions.length;
 };
 
+var QuizUI;
 
-var QuizUI = {
+function NewQuizUI(currentquiz) {
+    
+    delete QuizUI;
+    QuizUI = {
 
-    displayStart: function() {
+    displayStart: function(currentquiz) {
 
         console.log("in displayStart");
 
        try {
-                this.startHandler("start");
+                this.startHandler("start", currentquiz);
+                this.ec2Handler("ec2", currentquiz);
             }           
         catch(err) {
                 
-                QuizUI.displayNext();
-            }
-                 
-        
-
+                QuizUI.displayNext(currentquiz);
+            }    
     },
 
 
-    displayNext: function () {
+    displayNext: function (currentquiz) {
 
         console.log("in displayNext");
-        if (quiz.hasEnded()) {
-            this.displayScore();
+        if (currentquiz.hasEnded()) {
+            this.displayScore(currentquiz);
         } else {
-            this.displayQuestion();
-            this.displayChoices();
-            this.displayProgress();
+            this.displayQuestion(currentquiz);
+            this.displayChoices(currentquiz);
+            this.displayProgress(currentquiz);
         }
     },
-    displayQuestion: function() {
-        this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+    displayQuestion: function(currentquiz) {
+        this.populateIdWithHTML("question", currentquiz.getCurrentQuestion().text);
     },
-    displayChoices: function() {
-        var choices = quiz.getCurrentQuestion().choices;
+    displayChoices: function(currentquiz) {
+        var choices = currentquiz.getCurrentQuestion().choices;
 
         for(var i = 0; i < choices.length; i++) {
             this.populateIdWithHTML("choice" + i, choices[i]);
-            this.guessHandler("guess" + i, choices[i]);
+            this.guessHandler("guess" + i, choices[i], currentquiz);
         }
     },
-    displayScore: function() {
+    displayScore: function(currentquiz) {
         var gameOverHTML = "<h1>Game Over</h1>";
-        gameOverHTML += "<h2> Your score is: " + quiz.score + "</h2>";
+        gameOverHTML += "<h2> Your score is: " + currentquiz.score + "</h2>";
         this.populateIdWithHTML("quiz", gameOverHTML);
     },
     
@@ -77,26 +81,45 @@ var QuizUI = {
         var element = document.getElementById(id);
         element.innerHTML = text;
     },
-    guessHandler: function(id, guess) {
+    guessHandler: function(id, guess, currentquiz) {
         var button = document.getElementById(id);
         button.onclick = function() {
-            quiz.guess(guess);
-            QuizUI.displayNext();
+            currentquiz.guess(guess);
+            QuizUI.displayNext(currentquiz);
         }
     },
 
-    startHandler: function(id) {
+    startHandler: function(id, currentquiz) {
         var button = document.getElementById(id);
         
         if (button == null){
 
-            QuizUI.displayNext();
+            QuizUI.displayNext(currentquiz);
             
             } else {
 
              button.onclick = function() {
             
-               Start();
+               Start(currentquiz, topic);
+
+             }
+            
+            
+        }
+        
+    },
+    ec2Handler: function(id,curentquiz) {
+        var button = document.getElementById(id);
+        
+        if (button == null){
+
+            QuizUI.displayNext(currentquiz);
+            
+            } else {
+
+             button.onclick = function() {
+            
+               Ec2();
 
              }
             
@@ -105,14 +128,19 @@ var QuizUI = {
         
     },
     
-    displayProgress: function() {
-        var currentQuestionNumber = quiz.currentQuestionIndex + 1;
-        this.populateIdWithHTML("progress", "Question " + currentQuestionNumber + " of " + quiz.questions.length);
+    displayProgress: function(currentquiz) {
+        var currentQuestionNumber = currentquiz.currentQuestionIndex + 1;
+        this.populateIdWithHTML("progress", "Question " + currentQuestionNumber + " of " + currentquiz.questions.length);
     }
+
+  
 };
 
 
-function Question(text, choices, answer) {
+}
+
+function Question(topic, text, choices, answer) {
+    this.topic = topic;
     this.text = text;
     this.choices = choices;
     this.answer = answer;
@@ -126,9 +154,12 @@ Question.prototype.isCorrectAnswer = function (choice) {
 var allQuestions = new Array()
 var newquestions = new Array()
 var questions = new Array()
+var jsonPromise;
+var topic;
+
 var quiz
 
- var jsonPromise = $.getJSON('questions.json',function(data){
+function LoadAllQuestions() {jsonPromise = $.getJSON('questions.json',function(data){
         
         console.log('json loaded successfully');
         console.log(data)
@@ -140,7 +171,7 @@ var quiz
    
         allQuestions.forEach(function(element) {
         console.log(element);
-        questions.push(new Question(element.question, [ element.a, element.b, element.c, element.d ], element.correct))
+        questions.push(new Question(element.topic, element.question, [ element.a, element.b, element.c, element.d ], element.correct))
         
         
 });
@@ -149,23 +180,6 @@ var quiz
     }).error(function(){
         console.log('error: json not loaded');
     })
-
-
-
-
-
-function Start() {
-
-  
-
- window.location.href = '/quiz.html';
- QuizUI.displayNext();
- 
- 
-
- 
-
-}
 
 
 jsonPromise.done(function(data) {
@@ -178,7 +192,8 @@ jsonPromise.done(function(data) {
     });
 
     quiz = new Quiz(questions);
-    QuizUI.displayStart();
+    console.log(quiz)
+    topic = "all";
     
 });
 
@@ -191,8 +206,112 @@ jsonPromise.fail(function(reason) {
 jsonPromise.then(function(data) {
     // do moar stuff with data
     // will perhaps fire instantly, since the deferred may already be resolved.
-
+    NewQuizUI(quiz);
+    QuizUI.displayStart(quiz);
     
 });
+
+}
+
+
+function Start(currentquiz,topic) {
+ 
+ 
+ console.log(currentquiz)
+
+
+ window.location.href = '/quiz.html';
+
+ LoadAllQuestions();
+
+ QuizUI.displayNext(currentquiz);
+ 
+}
+
+
+
+
+//$('ec2').click(function () {
+    //callFunction();
+  //  console.log("clicked")
+//});
+
+function Ec2() {
+
+var ec2questions = new Array();
+var ec2quiz;
+
+
+console.log("in EC2");
+
+QuestionUpdate("EC2",function(result) 
+{
+       console.log(result)
+       ec2questions = result;
+});
+
+console.log("returned");
+
+
+NewQuiz(ec2questions,function(result) 
+{
+       console.log(result)
+       
+});
+
+
+
+
+QuizUI.displayStart(ec2quiz)
+
+//window.location.href = '/quiz.html';
+
+
+//Start(ec2quiz);
+
+//window.location.href = '/quiz.html';
+
+
+}
+
+function NewQuiz(currentquestions, callback) {
+
+    var newquiz;
+
+    newquiz = new Quiz(currentquestions);
+
+    console.log(newquiz);
+
+    NewQuizUI(newquiz);
+
+
+    callback(newquiz);
+
+
+}
+
+function QuestionUpdate(topic, callback)  {
+
+    console.log("In question update")
+
+   // update questions
+   var newquestions = new Array();
+   questions.forEach(function(element) {
+        console.log(element);
+        questions.push(new Question(element.question, [ element.a, element.b, element.c, element.d ], element.correct))
+        if(element.topic == topic) {
+
+            newquestions.push(element)
+
+        }
+        
+});
+
+   console.log(newquestions.length)
+   callback(newquestions)
+
+}
+
+
 
 
