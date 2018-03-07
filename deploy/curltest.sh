4d0241fb5c792cbc598849730339f09b7618acb4
John Cook <root@DESKTOP-HSQ9D3L.localdomain>
2018-03-06 21:25:08 -0700

    baseline add questions complete

---
+++ deploy/curltest.sh
@@ -0,0 +1,3 @@
+curl -v -H "Content-Type: application/json" -X POST \
+  -d '{"answer":"def sum(x,y):\n    return x+y"}' \
+  https://l3tr9t8tf1.execute-api.us-west-2.amazonaws.com/dev
--- deploy/staticdeploy.sh
+++ deploy/staticdeploy.sh
@@ -9,7 +9,7 @@ ASSET_BUCKET_NAME="newquizassets"
 
 aws s3 sync --acl 'public-read' --delete ../static/ "s3://${BUCKET_NAME}/"
 
-aws s3 sync --acl 'public-read'  ../js/ "s3://${ASSET_BUCKET_NAME}/"
+#aws s3 sync --acl 'public-read'  ../js/ "s3://${ASSET_BUCKET_NAME}/"
 aws s3 sync --acl 'public-read'  ../questions/ "s3://${ASSET_BUCKET_NAME}/"
 
 
